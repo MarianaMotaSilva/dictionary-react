24aded21d8f75afb90cc1419db8353a27c7db21e
Mariana <mariana.ssousasilva@gmail.com>
2024-10-02 16:21:40 +0200

    creating structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div claassName="container"></div>
       <header className="App-header">
         <p>Dictionary App</p>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">Coded by Mariana Mota da Silva</footer>
     </div>
   );
 }

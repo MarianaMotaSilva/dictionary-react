525a198899314f7d046010aa5238fa36c2bed2c9
Mariana <mariana.ssousasilva@gmail.com>
2024-10-10 18:19:50 +0200

    Added Results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useSate({});
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+    setResults(response.data[0]);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    if (keyword) {
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+      axios
+        .get(apiUrl)
+        .then(handleResponse)
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    } else {
+      console.error("Keyword is empty. Please enter a valid word.");
+    }
   }
 
   function handleKeywordChange(event) {
@@ -17,6 +36,7 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
+        <button type="submit">Search</button>{" "}
       </form>
     </div>
   );
